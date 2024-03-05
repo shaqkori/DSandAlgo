fb406136c240556fcf2887f9aec761caa53b4014
Shaqkori <mahoney.shaqkori@gmail.com>
2024-03-05 17:44:49 +0000

    remove element from array

---
+++ removeElement.js
@@ -0,0 +1,18 @@
+function removeElement (nums, val){
+
+    let k = 0 
+    
+    for(i = 0; i < nums.length; i++){
+        if (nums[i] !== val){
+            nums[k] = nums[i];
+            
+
+            k++;
+        }
+    }
+
+    return k
+}
+
+
+console.log(removeElement([3,2,2,3],3))
